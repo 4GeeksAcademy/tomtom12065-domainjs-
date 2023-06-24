25d273b5566304b1a65e1039b46982d9d3e1e2a5
tomtom12065 <100985000+tomtom12065@users.noreply.github.com>
2023-06-24 22:05:50 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// //
+// let pronoun = ["the", "our"];
+// let adj = ["great", "big"];
+// let noun = ["jogger", "racoon"];
+// //
+let generate = () => {
+  let pronoun = ["the", "our", "that"];
+  let adj = ["great", "big", "tiny"];
+  let noun = ["jogger", "racoon", "dog"];
+  let org = [".com", ".org", ".net"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let i = 0; i < pronoun.length; i++)
+    for (let j = 0; j < adj.length; j++)
+      for (let k = 0; k < noun.length; k++)
+        for (let l = 0; l < org.length; l++)
+          console.log(pronoun[i] + adj[j] + noun[k] + org[l]);
 };
